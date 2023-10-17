311a0a6c5e2fec3a6ab17beb1891003c2dee329a
idrissi-hamza <a.idrissihamza@gmail.com>
2023-10-17 12:31:54 +0100

    add reset button to reset filter query

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,6 +8,8 @@ interface ButtonProps {
   disabled?: boolean;
   rounded?: boolean;
   Icon?: IconType;
+  small?: boolean;
+  ghost?: boolean;
 }
 const Button = ({
   label,
@@ -15,24 +17,36 @@ const Button = ({
   disabled,
   rounded,
   Icon,
+  small,
+  ghost,
 }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={`disabled:opacity-70 disabled:cursor-not-allowed 
-                   flex items-center justify-center gap-4 px-4 py-2 text-md font-semibold 
-                  border-2 bg-slate-900 border-slate-900 text-white hover:opacity-90 transition
-         ${rounded ? 'rounded-full aspect-square' : 'rounded-lg w-full '}
+                 flex items-center justify-center  transition
+                 ${small ?
+          'py-1 px-3 gap-2 text-sm' :
+          'py-2 px-4 font-semibold  border-2 w-full gap-4'}
+                 ${ghost ?
+          ' text-slate-900 hover:opacity-90 hover:bg-gray-200/60' :
+          'bg-slate-900 border-slate-900 text-white hover:opacity-90'}
+                  
+                 ${rounded ?
+          'rounded-full aspect-square' :
+          'rounded-lg  '}
 
   `}
     >
+      {label}
       {Icon && (
         <Icon
-          size={30}
+          className={` ${small ?
+            'text-xl ' :
+            'text-4xl'}`}
         />
       )}
-      {label}
     </button>
   );
 };
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -8,6 +8,8 @@ import useTaskStore from '../hooks/useTaskStore';
 import Skeleton from './Skeleton';
 import SearchTask from './SearchTask';
 import toast from 'react-hot-toast';
+import { MdOutlineClear } from 'react-icons/md';
+import Button from './Button';
 
 const TodoList = () => {
   const { tasks, setTasks } = useTaskStore();
@@ -43,6 +45,9 @@ const TodoList = () => {
     setFilteredTasks(filtered);
   }, [tasks, searchQuery]);
 
+  const handleReset = () => {
+    setSearchQuery('');
+  };
 
 
   return (
@@ -51,8 +56,16 @@ const TodoList = () => {
         <h1 className="text-2xl font-bold ">Todo List</h1>
         <AddTaskButton />
       </div>
-      <div className='mb-4 px-10'>
+      <div className='mb-4 px-10 flex justify-start gap-2'>
         <SearchTask value={searchQuery} onChange={handleSearchChange} />
+        {searchQuery ?
+          <Button
+            onClick={handleReset}
+            label='Reset'
+            Icon={MdOutlineClear}
+            small
+            ghost
+          /> : null}
       </div>
       {isLoading ?
         <Skeleton />
